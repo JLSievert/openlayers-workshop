e82894416cfde1247335db2de4d744ae27f34454
ronaldboehm <github@f0ffff.net>
2019-02-05 16:49:33 +0100

    creating a map

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert('Hello Workshop');
+// alert('Hello Workshop');
+// from https://openlayers.org/workshop/en/basics/map.html
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZSource from 'ol/source/XYZ';
+import {fromLonLat} from 'ol/proj';
+
+const map = new Map({
+  target: 'map-container',
+  layers: [
+    new TileLayer({
+      source: new XYZSource({
+        url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+      })
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
